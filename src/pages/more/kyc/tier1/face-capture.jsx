10ac6d02ecc4b94a8ba1cc980fb0431b690776a2
Boladek <bmubarak88@gmail.com>
2024-05-30 16:43:05 +0100

    fixes

--- src/pages/more/kyc/tier1/face-capture.jsx
+++ src/pages/more/kyc/tier1/face-capture.jsx
@@ -16,7 +16,7 @@ export function FaceCapture({ open, handleClose, action, bvn }) {
 		setLoading(true);
 		kycAxios
 			.post("/kyc/verify-selfie-bvn", {
-				bvn: "22162666852" || bvn,
+				bvn: bvn,
 				selfieImage: imgSrc,
 			})
 			.then((res) => {
