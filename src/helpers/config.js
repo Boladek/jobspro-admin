efbaffeb9072723f6e3e61623e708dee368b5bdf
Boladek <bmubarak88@gmail.com>
2024-08-09 14:54:12 +0100

    fixes

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,13 +1,13 @@
 export const configKeys = {
 	googleId: import.meta.env.VITE_GOOGLE_ID,
-	baseURL:
-		import.meta.env.VITE_BASE_URL ||
-		"http://sso-lb-1694833758.us-west-2.elb.amazonaws.com",
+	baseURL: import.meta.env.VITE_BASE_URL,
+	SSOURL: import.meta.env.VITE_SSO_URL,
+	adminURL: import.meta.env.VITE_ADMIN_URL,
 	facebookId: import.meta.env.VITE_FB_ID,
 	facebookSecret: import.meta.env.VITE_FB_SECRET,
 	microsoftID: import.meta.env.VITE_MS_ID,
 	microsoftSecret: import.meta.env.VITE_MS_SECRET,
-	wsAddress: import.meta.env.VITE_WS_ADDRESS || "ws://54.191.23.207:10001",
-	apiAddress: import.meta.env.VITE_API_ADDRESS || "http://54.191.23.207:10002",
+	wsAddress: import.meta.env.VITE_WS_ADDRESS,
+	apiAddress: import.meta.env.VITE_API_ADDRESS,
 	placesApiID: import.meta.env.VITE_PLACES_ID,
 };
