52961c22e12efa5e34adb53c881581b21c2abece
Boladek <bmubarak88@gmail.com>
2025-02-18 08:28:19 +0100

    logs

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,22 @@ import StorageService from "./helpers/storage";
 import { auth } from "./helpers/auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "react-toastify/dist/ReactToastify.css";
+import { configKeys } from "./helpers/config";
+
+console.log({ configKeys });
 
 const queryClient = new QueryClient();
 
 if (auth()) {
-  const user = StorageService.getUser();
-  store.dispatch(loginSuccess(user));
+    const user = StorageService.getUser();
+    store.dispatch(loginSuccess(user));
 }
 
 export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ToastContainer />
-      <BaseRoutes />
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ToastContainer />
+            <BaseRoutes />
+        </QueryClientProvider>
+    );
 }
--- src/pages/push-notifications/publish-notification.jsx
+++ src/pages/push-notifications/publish-notification.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { useState } from "react";
 import { SideWrapper } from "../../component/side-wrapper";
 import { useForm } from "react-hook-form";
 import { checkIfFileIsImage, generateArray } from "../../helpers/function";
